4ac79959ab9def273c3521d529f877b06ad3fb42
Aaron Tsui <aaron.tsui@outlook.com>
2019-03-04 14:15:37 +0800

    open serial port and print.
    Signed-off-by: Aaron Tsui <aaron.tsui@outlook.com>

--- app/mesh.py
+++ app/mesh.py
@@ -3,6 +3,10 @@ import cayenne.client
 import time
 import logging
 
+import serial
+from time import sleep  
+
+
 # Cayenne authentication info. This should be obtained from the Cayenne Dashboard.
 MQTT_USERNAME  = "1659fa40-393b-11e9-b85b-afa01d2216d6"
 MQTT_PASSWORD  = "31b38212a4354b59a4bc5c2a0ed9d86174c5b6c8"
@@ -17,6 +21,16 @@ client.begin(MQTT_USERNAME, MQTT_PASSWORD, MQTT_CLIENT_ID, loglevel=logging.INFO
 i=0
 timestamp = 0
 
+ser = serial.Serial('COM14', # Device name varies
+                    baudrate=115200,
+                    bytesize=8,
+                    parity='N',
+                    stopbits=1,
+                    timeout=1)
+                    
+print (ser.portstr)       # check which port was really used  
+        
+        
 while True:
     client.loop()
     
@@ -26,3 +40,7 @@ while True:
         client.hectoPascalWrite(3, i+800)
         timestamp = time.time()
         i = i+1
+    
+    line = ser.readline()# read a '\n' terminated line
+    if(line!=b''):
+        print (str(line, encoding = "utf-8"))
